6a7e44133d0bcebbddaeefc0aac786832d46a7e2
Heqing Ya <normanyahq@gmail.com>
2016-08-15 17:04:57 -0400

    sdf

--- js/threebody.js
+++ js/threebody.js
@@ -23,7 +23,7 @@ pos2.divide(new Victor(pos_scale, pos_scale));
 pos3.divide(new Victor(pos_scale, pos_scale));
 
 var v1 = new Victor(1 + Math.random()/10, 1 - Math.random()/10);
-var v2 = new Victor(-1 - Math.random()/10, 1 + Math.random()/10);
+var v2 = new Victor(-1 - Math.random()/10, -1 + Math.random()/10);
 v1.normalize();
 v2.normalize();
 //ensure the momentum of the whole system is zero.
